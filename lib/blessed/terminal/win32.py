3f673865b2cfbb13ddc8e51f6b56f029e8ab43ff
Chris Travers <chris@efficito.com>
2015-12-28 15:26:20 +0100

    basic api design for terminal abstraction

---
+++ lib/blessed/terminal/win32.py
@@ -0,0 +1,14 @@
+
+def cbreak(terminal):
+    """Sets cbreak mode for current terminal"""
+
+def buffered(terminal):
+    """Sets bufferred mode for current terminal"""
+
+def get_size(terminal):
+    """Gets he size of the current terminal"""
+
+def echo(terminal, echo_bool):
+    """Sets the terminal to echo output or not"""
+
+
