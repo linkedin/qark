e08539210d147c406af59fa6557b783738cb4d31
Alexey Shatlovsky <alexey.shatlovsky@booking.com>
2016-02-10 16:59:06 +0100

    Procyon library updated to 0.5.30

--- modules/unpackAPK.py
+++ modules/unpackAPK.py
@@ -247,7 +247,7 @@ def procyon(path,dirname):
 	"""
 	calls the procyon decompiler from command line
 	"""
-	process = subprocess.Popen(["java","-jar", common.rootDir + "/lib/procyon/procyon-decompiler-0.5.29.jar", path, "-o ", dirname+"2"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+	process = subprocess.Popen(["java","-jar", common.rootDir + "/lib/procyon/procyon-decompiler-0.5.30.jar", path, "-o ", dirname+"2"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 	try:
 		while True:
 			line = process.stdout.readline()
