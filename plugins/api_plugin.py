671e3891402e6231851e8d6977ae606201f8fb7d
Tushar Dalvi <tushardalvi@gmail.com>
2016-06-29 14:15:11 -0700

    updated plugin templates

--- plugins/api_plugin.py
+++ plugins/api_plugin.py
@@ -12,15 +12,22 @@ from lib.pubsub import pub
 import logging
 
 
-class PluginOne(IPlugin):
+class PluginTwo(IPlugin):
     def target(self, queue):
         results = []
+        #TODO: add documentation for available API calls. Sample shown below.
+        # Here, we want to scan all decompiled files to see if any file contains the text "API_KEY"
         possibleFiles = common.text_scan(common.java_files, r'API_KEY')
         count = 0
         for f in possibleFiles:
             count += 1
+            # The following call generates the progress bar in the terminal output
             pub.sendMessage('progress', bar=self.getName(), percent=round(count*100/len(possibleFiles)))
+
+            # Mostly for logging. This goes in the log file generated under /logs
             common.logger.debug("Text found, " + str(f))
+
+            # This will put individual results of the plugin scan in the HTML report.
             issue = ReportIssue()
             issue.setCategory(ExploitType.PLUGIN)
             issue.setDetails("The string 'API_KEY' appears in the file: %s\n%s" % (f[1], str(f[0])))
@@ -28,20 +35,24 @@ class PluginOne(IPlugin):
             issue.setSeverity(Severity.VULNERABILITY)
             results.append(issue)
 
+            # This puts individual results of the plugin scan in the terminal output.
             issue = terminalPrint()
             issue.setLevel(Severity.VULNERABILITY)
             issue.setData("The string 'API_KEY' appears in the file: %s\n%s" % (f[1], str(f[0])))
             results.append(issue)
-        
-        
+
+        # This is required to send the complete list of results (including the ones to be printed on terminal as well as
+        # issues to be printed in tht HTML report) back to the main thread.
         queue.put(results)
             
 
     def getName(self):
-        return "Hardcoded API Key Check"
+        # The name to be displayed against the progressbar
+        return "Hardcoded API Keys"
 
     def getCategory(self):
-        return "Hardcoded API keys"
+        # Currently unused, but will be used later for clubbing issues from a specific plugin (when multiple plugins run at the same time)
+        return "PLUGIN ISSUES"
 
     def getTarget(self):
         return self.target
