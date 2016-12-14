5d2d82a0915a90c3310af84d5b3a17afc77aaa91
neilatli <neilatli@users.noreply.github.com>
2016-12-14 13:59:09 -0800

    Mocking os.environ for test_common (#69)
    * Mocking os.environ
    
    * Fixing the case where ANDROID_HOME is not in os.environ

--- qark/test/test_common.py
+++ qark/test/test_common.py
@@ -12,9 +12,11 @@ def mocked_getConfig():
 @mock.patch('common.getConfig', return_value=mocked_getConfig())
 def test_set_environment_variables(mocked):
     common.rootDir = './'
-    assert 'ANDROID_HOME' not in os.environ
-    common.set_environment_variables()
-    assert 'ANDROID_HOME' in os.environ
+    with mock.patch.dict('os.environ'):
+        os.environ.pop('ANDROID_HOME', None)
+        assert 'ANDROID_HOME' not in os.environ
+        common.set_environment_variables()
+        assert 'ANDROID_HOME' in os.environ
 
 def test_dedup():
     d = common.dedup
