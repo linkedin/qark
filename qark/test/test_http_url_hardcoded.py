77516982d4eb8ce54dbb1c66ce8bdce645539f9c
sarveshkapre <kapre.s@husky.neu.edu>
2017-06-28 10:26:59 -0700

    http test case

--- qark/test/test_http_url_hardcoded.py
+++ qark/test/test_http_url_hardcoded.py
@@ -12,8 +12,11 @@ def testuri_regex1():
 def testuri_regex2():
     assert PluginUtil.contains(plugin.http_url_regex, 'www.xyz.in') is False
 
+def testuri_regex3():
+    assert PluginUtil.contains(plugin.http_url_regex, 'https://www.linkedin.com/') is False
 
 if __name__ == '__main__':
     testuri_regex()
     testuri_regex1()
     testuri_regex2()
+    testuri_regex3()
