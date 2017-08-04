b24c15f0f4be85598aec91e78e6a056953dc6895
sarveshkapre <kapre.s@husky.neu.edu>
2017-08-04 16:24:05 -0700

    PR changes

--- qark/plugins/insecure_functions.py
+++ qark/plugins/insecure_functions.py
@@ -1,4 +1,6 @@
-import sys, os, re
+import sys
+import os
+import re
 sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)) + '../lib')
 from yapsy.IPlugin import IPlugin
 from plugins import PluginUtil
@@ -14,41 +16,41 @@ class InsecureFunctionsPlugin(IPlugin):
         self.name = 'Insecure functions'
 
     # recursive function to check for the function name by traversing down the AST
-    def recursive_insecure_call_function(self, t, filename, res):
-        if type(t) is m.MethodDeclaration:
-            if str(t.name) == self.CALL_FUNCTION:
-                PluginUtil.reportInfo(fileName, self.InsecureFunctionIssueDetails(fileName), res)
-        elif type(t) is list:
-            for x in t:
-                self.recursive_insecure_call_function(x, filename, res)
-        elif hasattr(t, '_fields'):
-            for f in t._fields:
-                self.recursive_insecure_call_function(getattr(t, f), filename, res)
+    def recursive_insecure_call_function(self, fields, file, res):
+        if type(fields) is m.MethodDeclaration:
+            if str(fields.name) == self.CALL_FUNCTION:
+                PluginUtil.reportInfo(file_name, self.insecure_function_issue(file_name), res)
+        elif type(fields) is list:
+            for fieldname in fields:
+                self.recursive_insecure_call_function(fieldname, file, res)
+        elif hasattr(fields, '_fields'):
+            for fieldname in fields._fields:
+                self.recursive_insecure_call_function(getattr(fields, fieldname), file, res)
         return
 
     def target(self, queue):
         files = common.java_files
-        global parser, tree, fileName
+        global parser, tree, file_name
         parser = plyj.Parser()
         tree = ''
         global res
         res = []
 
         count = 0
-        for f in files:
+        for file in files:
             count += 1
             pub.sendMessage('progress', bar=self.name, percent=round(count * 100 / len(files)))
-            fileName = str(f)
+            file_name = str(file)
             try:
-                tree = parser.parse_file(f)
+                tree = parser.parse_file(file)
             except Exception:
                 continue
             try:
                 for type_decl in tree.type_declarations:
                     if type(type_decl) is m.ClassDeclaration:
-                        for t in type_decl.body:
+                        for fields in type_decl.body:
                             try:
-                                self.recursive_insecure_call_function(t, f, res)
+                                self.recursive_insecure_call_function(fields, file, res)
 
                             except Exception as e:
                                 common.logger.error(
@@ -58,7 +60,7 @@ class InsecureFunctionsPlugin(IPlugin):
                 continue
         queue.put(res)
 
-    def InsecureFunctionIssueDetails(self, fileName):
+    def insecure_function_issue(self, file_name):
         return 'The Content provider API provides a method call\n' \
                'The framework does no permission checking on this entry into the content provider besides the basic ability ' \
                'for the application to get access to the provider at all. \n' \
@@ -66,7 +68,7 @@ class InsecureFunctionsPlugin(IPlugin):
                'Failure to do so will allow unauthorized components to interact with the content provider.\n' \
                'Filepath: %s\n' \
                'Reference: https://bitbucket.org/secure-it-i/android-app-vulnerability-benchmarks/src/d5305b9481df3502e60e98fa352d5f58e4a69044/ICC/WeakChecksOnDynamicInvocation-InformationExposure/?at=master \n' \
-               % fileName
+               % file_name
 
     def getName(self):
         return "Insecure functions"
--- qark/test/test_insecure_functions.py
+++ qark/test/test_insecure_functions.py
@@ -5,16 +5,15 @@ plugin = InsecureFunctionsPlugin()
 
 def test_regex():
     text = 'call'
-    assert PluginUtil.contains(plugin.CALL_FUNCTION, text) is True
+    assert PluginUtil.contains(plugin.CALL_FUNCTION, text)
 
 def test_regex1():
     text = 'call(String method, String args, Bundle extras)'
-    assert PluginUtil.contains(plugin.CALL_FUNCTION, text) is True
+    assert PluginUtil.contains(plugin.CALL_FUNCTION, text)
 
 def test_regex2():
     text = 'call()'
-    assert PluginUtil.contains(plugin.CALL_FUNCTION, text) is True
-
+    assert PluginUtil.contains(plugin.CALL_FUNCTION, text)
 
 if __name__ == '__main__':
     test_regex()
