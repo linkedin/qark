20647bb1e380d222ffd5e22b4df5206058989af2
sarveshkapre <kapre.s@husky.neu.edu>
2017-08-04 17:44:18 -0700

    PR changes

--- qark/plugins/webview_checks.py
+++ qark/plugins/webview_checks.py
@@ -1,4 +1,6 @@
-import sys, os, re
+import sys
+import os
+import re
 
 sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)) + '../lib')
 
@@ -25,16 +27,17 @@ class WebViewChecksPlugin(IPlugin):
         global parser
         parser = plyj.Parser()
         global tree
-        global fileName
+        global file_name
         tree = ''
         res = []
+        issues_list =[]
         count = 0
-        for f in files:
+        for file in files:
             count += 1
             pub.sendMessage('progress', bar=self.name, percent=round(count * 100 / len(files)))
-            fileName = str(f)
+            file_name = str(file)
             try:
-                tree = parser.parse_file(f)
+                tree = parser.parse_file(file)
             except Exception:
                 continue
 
@@ -42,19 +45,22 @@ class WebViewChecksPlugin(IPlugin):
                 global url
                 url = []
                 for import_decl in tree.import_declarations:
+                    # Check import statements with value declared as WebView and WebSettings for the potential use of web views
                     if 'WebView' in import_decl.name.value or 'WebSettings' in import_decl.name.value:
-                        fileBody = str(open(fileName, 'r').read())
+                        with open(file_name, 'r') as f:
+                            data = f.read()
 
-                        if PluginUtil.contains(self.JAVASCRIPT_ENABLED, fileBody):
+                        if PluginUtil.contains(self.JAVASCRIPT_ENABLED, data):
 
-                            if PluginUtil.contains(self.MIXED_CONTENT, fileBody):
-                                PluginUtil.reportWarning(fileName, self.MixedContentIssueDetails(fileName), res)
+                            if PluginUtil.contains(self.MIXED_CONTENT, data):
+                                PluginUtil.reportWarning(file_name, self.mixed_content_issue(file_name), res)
 
-                            if "setAllowFileAccess(false)" or "setAllowContentAccess(false)" not in fileBody:
-                                PluginUtil.reportInfo(fileName, self.SecureContentIssueDetails(fileName), res)
+                            if "setAllowFileAccess(false)" or "setAllowContentAccess(false)" not in data:
+                                if not file_name in issues_list:
+                                    issues_list.append(file_name)
 
-                        if PluginUtil.contains(self.LOAD_URL_HTTP, fileBody):
-                            PluginUtil.reportWarning(fileName, self.loadhttpurlsIssueDetails(fileName), res)
+                        if PluginUtil.contains(self.LOAD_URL_HTTP, data):
+                            PluginUtil.reportWarning(file_name, self.load_http_urls(file_name), res)
 
                         break
 
@@ -65,17 +71,20 @@ class WebViewChecksPlugin(IPlugin):
                 for import_decl in tree.import_declarations:
                     if 'WebView' in import_decl.name.value or 'WebSettings' in import_decl.name.value:
                         for type_decl in tree.type_declarations:
+                            # Check for class declaration in java source code and traverse further down the AST to find method names
                             if type(type_decl) is m.ClassDeclaration:
-                                for t in type_decl.body:
-                                    if type(t) is m.MethodDeclaration:
-                                        if 'shouldOverrideUrlLoading' in t.name:
-                                            if 'true' not in str(t.body):
-                                                PluginUtil.reportWarning(fileName, self.OverrideUrlIssueDetails(fileName), res)
+                                for fields in type_decl.body:
+                                    if type(fields) is m.MethodDeclaration:
+                                        if 'shouldOverrideUrlLoading' in fields.name:
+                                            if 'true' not in str(fields.body):
+                                                PluginUtil.reportWarning(file_name, self.url_override_issue(file_name), res)
+                                                break
                                             else:
                                                 continue
-                                        if 'shouldInterceptRequest' in t.name:
-                                            if 'null' in str(t.body):
-                                                PluginUtil.reportWarning(fileName, self.InterceptRequestIssueDetails(fileName), res)
+                                        if 'shouldInterceptRequest' in fields.name:
+                                            if 'null' in str(fields.body):
+                                                PluginUtil.reportWarning(file_name, self.intercept_request_issue(file_name), res)
+                                                break
                                             else:
                                                 continue
                         break
@@ -83,40 +92,43 @@ class WebViewChecksPlugin(IPlugin):
             except Exception as e:
                 continue
 
+        if issues_list:
+            issue_name = " \n".join(issues_list)
+            PluginUtil.reportInfo(file_name, self.secure_content_issue(issue_name), res)
+
         queue.put(res)
 
-    def loadhttpurlsIssueDetails(self, fileName):
+    def load_http_urls(self, file_name):
         return 'If WebView is allowing to load clear-text content from the Internet\n ' \
                'then it would be open to various forms of attack such as MiTM. \n%s\n' \
-                % fileName
+                % file_name
 
-    def MixedContentIssueDetails(self, fileName):
+    def mixed_content_issue(self, file_name):
         return 'Usage of setMixedContentMode is found\n' \
                'In this mode, the WebView will allow a secure origin to load content ' \
                'from any other origin, even if that origin is insecure. ' \
                'This is the least secure mode of operation for the WebView, ' \
                'and where possible apps should not set this mode.\n ' \
                'https://developer.android.com/reference/android/webkit/WebSettings.html#MIXED_CONTENT_ALWAYS_ALLOW \n%s\n'\
-                % fileName
+                % file_name
 
-    def SecureContentIssueDetails(self, fileName):
+    def secure_content_issue(self, issue_name):
         return 'File System Access is by default enabled\n' \
                'setAllowFileAccess() and setAllowContentAccess() are by default true. ' \
                'This should be set to false to restrict access to local data since it is used to display content from locally stored HTML ' \
-               'or fetch HTML and other content from the server.\n ' \
+               'or fetch HTML and other content from the server.\n' \
                'https://developer.android.com/reference/android/webkit/WebSettings.html \n%s\n' \
-                % fileName
+                % issue_name
 
-
-    def OverrideUrlIssueDetails(self, fileName):
+    def url_override_issue(self, file_name):
         return 'Improper implementation of shouldOverrideUrlLoading method\n' \
                'This incorrect implementation allows any url to load in the webview \n%s\n' \
-                % fileName
+                % file_name
 
-    def InterceptRequestIssueDetails(self, fileName):
+    def intercept_request_issue(self, file_name):
         return 'Improper implementation of shouldInterceptRequest method\n' \
                'Returning null allows any url to load in the webview \n%s\n' \
-                % fileName
+                % file_name
 
     def getName(self):
         return "Webview Issues"
--- qark/test/test_webview_checks.py
+++ qark/test/test_webview_checks.py
@@ -5,35 +5,35 @@ plugin = WebViewChecksPlugin()
 
 def test_regex1():
     text = 'setJavaScriptEnabled(false)'
-    assert PluginUtil.contains(plugin.JAVASCRIPT_ENABLED, text) is False
+    assert not PluginUtil.contains(plugin.JAVASCRIPT_ENABLED, text)
 
 def test_regex2():
     text = 'setJavaScriptEnabled(true)'
-    assert PluginUtil.contains(plugin.JAVASCRIPT_ENABLED, text) is True
+    assert PluginUtil.contains(plugin.JAVASCRIPT_ENABLED, text)
 
 def test_regex3():
     text = 'loadUrl("https:'
-    assert PluginUtil.contains(plugin.LOAD_URL_HTTP, text) is False
+    assert not PluginUtil.contains(plugin.LOAD_URL_HTTP, text)
 
 def test_regex4():
     text = 'loadUrl("http:'
-    assert PluginUtil.contains(plugin.LOAD_URL_HTTP, text) is True
+    assert PluginUtil.contains(plugin.LOAD_URL_HTTP, text)
 
 def test_regex5():
     text = 'setMixedContentMode'
-    assert PluginUtil.contains(plugin.MIXED_CONTENT, text) is True
+    assert PluginUtil.contains(plugin.MIXED_CONTENT, text)
 
 def test_regex6():
     text = 'SetMixedContentMode'
-    assert PluginUtil.contains(plugin.MIXED_CONTENT, text) is False
+    assert not PluginUtil.contains(plugin.MIXED_CONTENT, text)
 
 def test_regex7():
     text = "loadUrl('https:"
-    assert PluginUtil.contains(plugin.LOAD_URL_HTTP, text) is False
+    assert not PluginUtil.contains(plugin.LOAD_URL_HTTP, text)
 
 def test_regex8():
     text = "loadUrl('http:"
-    assert PluginUtil.contains(plugin.LOAD_URL_HTTP, text) is True
+    assert PluginUtil.contains(plugin.LOAD_URL_HTTP, text)
 
 
 if __name__ == '__main__':
