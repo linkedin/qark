ea6a7bed6e5b0b32268d8b76d7133b7779c5f987
sarveshkapre <kapre.s@husky.neu.edu>
2017-08-10 16:28:21 -0700

    pep8

--- qark/plugins/insecure_functions.py
+++ qark/plugins/insecure_functions.py
@@ -1,13 +1,23 @@
-import sys
-import os
-import re
-sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)) + '../lib')
 from yapsy.IPlugin import IPlugin
 from plugins import PluginUtil
 from modules import common
 from lib.pubsub import pub
+import sys
+import os
 import lib.plyj.parser as plyj
 import lib.plyj.model as m
+
+sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)) + '../lib')
+
+
+def insecure_function(file_name):
+    string = ("The Content provider API provides a method call\nThe framework does no permission checking on this "
+              "entry into the content provider besides the basic ability for the application to get access to the provider"
+              " at all. Any implementation of this method must do its own permission checks on incoming calls to make sure "
+              "they are allowed.Failure to do so will allow unauthorized components to interact with the content provider.\nFilepath: {}\n"
+              "Reference: https://bitbucket.org/secure-it-i/android-app-vulnerability-benchmarks/src/d5305b9481df3502e60e98fa352d5f58e4a69044/ICC/WeakChecksOnDynamicInvocation-InformationExposure/?at=master")
+    return string.format(file_name)
+
 
 class InsecureFunctionsPlugin(IPlugin):
     CALL_FUNCTION = r'call'
@@ -19,7 +29,7 @@ class InsecureFunctionsPlugin(IPlugin):
     def recursive_insecure_call_function(self, fields, file, res):
         if type(fields) is m.MethodDeclaration:
             if str(fields.name) == self.CALL_FUNCTION:
-                PluginUtil.reportInfo(file_name, self.insecure_function(file_name), res)
+                PluginUtil.reportInfo(file_name, insecure_function(file_name), res)
         elif type(fields) is list:
             for fieldname in fields:
                 self.recursive_insecure_call_function(fieldname, file, res)
@@ -37,43 +47,35 @@ class InsecureFunctionsPlugin(IPlugin):
         res = []
 
         count = 0
-        for file in files:
+        for f in files:
             count += 1
             pub.sendMessage('progress', bar=self.name, percent=round(count * 100 / len(files)))
-            file_name = str(file)
+            file_name = str(f)
             try:
-                tree = parser.parse_file(file)
-            except Exception:
-                continue
+                tree = parser.parse_file(f)
+            except Exception as e:
+                common.logger.exception(
+                    "Unable to parse the file and generate as AST. Error: " + str(e))
             try:
                 for type_decl in tree.type_declarations:
                     if type(type_decl) is m.ClassDeclaration:
                         for fields in type_decl.body:
                             try:
-                                self.recursive_insecure_call_function(fields, file, res)
-
+                                self.recursive_insecure_call_function(fields, f, res)
                             except Exception as e:
                                 common.logger.exception(
                                     "Unable to run insecure function plugin " + str(e))
 
             except Exception:
-                continue
+                pass
         queue.put(res)
 
-    def insecure_function(self, file_name):
-        return 'The Content provider API provides a method call\n' \
-               'The framework does no permission checking on this entry into the content provider besides the basic ability ' \
-               'for the application to get access to the provider at all. \n' \
-               'Any implementation of this method must do its own permission checks on incoming calls to make sure they are allowed. ' \
-               'Failure to do so will allow unauthorized components to interact with the content provider.\n' \
-               'Filepath: %s\n' \
-               'Reference: https://bitbucket.org/secure-it-i/android-app-vulnerability-benchmarks/src/d5305b9481df3502e60e98fa352d5f58e4a69044/ICC/WeakChecksOnDynamicInvocation-InformationExposure/?at=master \n' \
-               % file_name
-
-    def getName(self):
+    @staticmethod
+    def getName():
         return "Insecure functions"
 
-    def getCategory(self):
+    @staticmethod
+    def getCategory():
         # Currently unused, but will be used later for clubbing issues from a specific plugin (when multiple plugins run at the same time)
         return "PLUGIN ISSUES"
 
