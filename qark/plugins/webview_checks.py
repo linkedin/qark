9df9a6ef4319541f58a2fff98234877fa223a7ed
sarveshkapre <kapre.s@husky.neu.edu>
2017-08-10 17:03:22 -0700

    pep8

--- qark/plugins/webview_checks.py
+++ qark/plugins/webview_checks.py
@@ -1,15 +1,49 @@
-import sys
-import os
-import re
-
-sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)) + '../lib')
-
 from yapsy.IPlugin import IPlugin
 from plugins import PluginUtil
 from modules import common
 from lib.pubsub import pub
+import sys
+import os
 import lib.plyj.parser as plyj
 import lib.plyj.model as m
+
+sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)) + '../lib')
+
+
+def intercept_request(file_name):
+    string = ("Improper implementation of shouldInterceptRequest method\n"
+              "Returning null allows any url to load in the webview\nFilepath: {}\n")
+    return string.format(file_name)
+
+
+def url_override(file_name):
+    string = ("Improper implementation of shouldOverrideUrlLoading method\n"
+              "This incorrect implementation allows any url to load in the webview\nFilepath: {}\n")
+    return string.format(file_name)
+
+
+def secure_content(issue_name):
+    string = ("File System Access is by default enabled\n"
+              "setAllowFileAccess() and setAllowContentAccess() are by default true. This should"
+              " be set to false to restrict access to local data since it is used to display"
+              " content from locally stored HTML or fetch HTML and other content from the server.\n"
+              "Reference: https://developer.android.com/reference/android/webkit/WebSettings.html\nFilepath: {}\n")
+    return string.format(issue_name)
+
+
+def load_http_urls(file_name):
+    string = ("Webview is loading http urls\nIf WebView is allowing to load clear-text content"
+              " from the Internet then it would be open to various forms of attack such as MiTM.\nFilepath: {}\n")
+    return string.format(file_name)
+
+
+def mixed_content(file_name):
+    string = ("Usage of setMixedContentMode is found\n"
+              "In this mode, the WebView will allow a secure origin to load content from"
+              " any other origin, even if that origin is insecure. This is the least secure"
+              " mode of operation for the WebView, and where possible apps should not set this mode.\n"
+              "Reference: https://developer.android.com/reference/android/webkit/WebSettings.html#MIXED_CONTENT_ALWAYS_ALLOW \nFilepath: {}\n")
+    return string.format(file_name)
 
 
 class WebViewChecksPlugin(IPlugin):
@@ -23,22 +57,21 @@ class WebViewChecksPlugin(IPlugin):
 
     def target(self, queue):
         files = common.java_files
-        global parser
         parser = plyj.Parser()
-        global tree
         global file_name
         tree = ''
         res = []
         issues_list = []
         count = 0
-        for file in files:
+        for f in files:
             count += 1
             pub.sendMessage('progress', bar=self.name, percent=round(count * 100 / len(files)))
-            file_name = str(file)
+            file_name = str(f)
             try:
-                tree = parser.parse_file(file)
-            except Exception:
-                continue
+                tree = parser.parse_file(f)
+            except Exception as e:
+                common.logger.exception(
+                    "Unable to parse the file and generate as AST. Error: " + str(e))
 
             try:
                 global url
@@ -46,24 +79,23 @@ class WebViewChecksPlugin(IPlugin):
                 for import_decl in tree.import_declarations:
                     # Check import statements with value declared as WebView and WebSettings for the potential use of web views
                     if 'WebView' in import_decl.name.value or 'WebSettings' in import_decl.name.value:
-                        with open(file_name, 'r') as f:
-                            data = f.read()
+                        with open(file_name, 'r') as r:
+                            data = r.read()
 
                         if PluginUtil.contains(self.JAVASCRIPT_ENABLED, data):
-
                             if PluginUtil.contains(self.MIXED_CONTENT, data):
-                                PluginUtil.reportWarning(file_name, self.mixed_content_issue(file_name), res)
+                                PluginUtil.reportWarning(file_name, mixed_content(file_name), res)
 
                             if "setAllowFileAccess(false)" or "setAllowContentAccess(false)" not in data:
-                                if not file_name in issues_list:
+                                if file_name not in issues_list:
                                     issues_list.append(file_name)
 
                         if PluginUtil.contains(self.LOAD_URL_HTTP, data):
-                            PluginUtil.reportWarning(file_name, self.load_http_urls(file_name), res)
+                            PluginUtil.reportWarning(file_name, load_http_urls(file_name), res)
                         break
 
             except Exception:
-                continue
+                pass
 
             try:
                 for import_decl in tree.import_declarations:
@@ -75,59 +107,26 @@ class WebViewChecksPlugin(IPlugin):
                                     if type(fields) is m.MethodDeclaration:
                                         if 'shouldOverrideUrlLoading' in fields.name:
                                             if 'true' not in str(fields.body):
-                                                PluginUtil.reportWarning(file_name, self.url_override_issue(file_name),
-                                                                         res)
+                                                PluginUtil.reportWarning(file_name, url_override(file_name), res)
                                                 break
                                             else:
                                                 continue
                                         if 'shouldInterceptRequest' in fields.name:
                                             if 'null' in str(fields.body):
-                                                PluginUtil.reportWarning(file_name,
-                                                                         self.intercept_request_issue(file_name), res)
+                                                PluginUtil.reportWarning(file_name, intercept_request(file_name), res)
                                                 break
                                             else:
                                                 continue
                         break
 
             except Exception:
-                continue
+                pass
 
         if issues_list:
             issue_name = " \n".join(issues_list)
-            PluginUtil.reportInfo(file_name, self.secure_content_issue(issue_name), res)
+            PluginUtil.reportInfo(file_name, secure_content(issue_name), res)
 
         queue.put(res)
-
-    def load_http_urls(self, file_name):
-        string = ("Webview is loading http urls\nIf WebView is allowing to load clear-text content"
-                  " from the Internet then it would be open to various forms of attack such as MiTM.\nFilepath: {}\n")
-        return string.format(file_name)
-
-    def mixed_content_issue(self, file_name):
-        string = ("Usage of setMixedContentMode is found\n"
-                  "In this mode, the WebView will allow a secure origin to load content from"
-                  " any other origin, even if that origin is insecure. This is the least secure"
-                  " mode of operation for the WebView, and where possible apps should not set this mode.\n"
-                  "Reference: https://developer.android.com/reference/android/webkit/WebSettings.html#MIXED_CONTENT_ALWAYS_ALLOW \nFilepath: {}\n")
-        return string.format(file_name)
-
-    def secure_content_issue(self, issue_name):
-        string = ("File System Access is by default enabled\n"
-                  "setAllowFileAccess() and setAllowContentAccess() are by default true. This should"
-                  " be set to false to restrict access to local data since it is used to display"
-                  " content from locally stored HTML or fetch HTML and other content from the server.\n"
-                  "Reference: https://developer.android.com/reference/android/webkit/WebSettings.html\nFilepath: {}\n")
-        return string.format(issue_name)
-
-    def url_override_issue(self, file_name):
-        string = ("Improper implementation of shouldOverrideUrlLoading method\n"
-                  "This incorrect implementation allows any url to load in the webview\nFilepath: {}\n")
-        return string.format(file_name)
-
-    def intercept_request_issue(self, file_name):
-        string = ("Improper implementation of shouldInterceptRequest method\n"
-                  "Returning null allows any url to load in the webview\nFilepath: {}\n")
-        return string.format(file_name)
 
     def getName(self):
         return "Webview Issues"
