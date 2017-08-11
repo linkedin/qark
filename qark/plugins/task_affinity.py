4776eaea997a627194eaa7c593f6fc79bb554cd9
sarveshkapre <kapre.s@husky.neu.edu>
2017-08-10 17:29:26 -0700

    pep8

--- qark/plugins/task_affinity.py
+++ qark/plugins/task_affinity.py
@@ -1,17 +1,27 @@
-import sys
-import os
-import re
-
-sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)) + '../lib')
-
 from yapsy.IPlugin import IPlugin
 from plugins import PluginUtil
 from modules import common
 from lib.pubsub import pub
+import sys
+import os
 import lib.plyj.parser as plyj
 
-class TaskAffinityPlugin(IPlugin):
+sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)) + '../lib')
 
+string = ("FLAG_ACTIVITY_{}_TASK - intent flag is set.\nThis results in activity being loaded"
+          "as a part of a new task. This can be abused in the task hijacking attack.\n{}\n"
+          "Reference: https://www.usenix.org/system/files/conference/usenixsecurity15/sec15-paper-ren-chuangang.pdf\n")
+
+
+def new_task(file_path):
+    return string.format("NEW", file_path)
+
+
+def multiple_task(file_path):
+    return string.format("MULTIPLE", file_path)
+
+
+class TaskAffinityPlugin(IPlugin):
     NEW_TASK = r'FLAG\_ACTIVITY\_NEW\_TASK'
     MULTIPLE_TASK = r'FLAG\_ACTIVITY\_MULTIPLE\_TASK'
 
@@ -20,55 +30,44 @@ class TaskAffinityPlugin(IPlugin):
 
     def target(self, queue):
         files = common.java_files
-        global parser, tree, file_name
+        global filepath
         parser = plyj.Parser()
         tree = ''
         res = []
         count = 0
-        for file in files:
+        for f in files:
             count += 1
             pub.sendMessage('progress', bar=self.name, percent=round(count * 100 / len(files)))
-            file_name = str(file)
+            filepath = str(f)
             try:
-                tree = parser.parse_file(file)
-            except Exception:
-                continue
+                tree = parser.parse_file(f)
+            except Exception as e:
+                common.logger.exception(
+                    "Unable to parse the file and generate as AST. Error: " + str(e))
 
             try:
                 for import_decl in tree.import_declarations:
                     # Check if Intent is called in the import statement
                     if 'Intent' in import_decl.name.value:
-                        with open(file_name, 'r') as f:
-                            file_body = f.read()
+                        with open(filepath, 'r') as r:
+                            file_body = r.read()
                         if PluginUtil.contains(self.NEW_TASK, file_body):
-                            PluginUtil.reportWarning(file_name, self.new_task_issue(file_name), res)
+                            PluginUtil.reportInfo(filepath, new_task(filepath), res)
                             break
                         if PluginUtil.contains(self.MULTIPLE_TASK_TASK, file_body):
-                            PluginUtil.reportWarning(file_name, self.multiple_task_issue(file_name), res)
+                            PluginUtil.reportInfo(filepath, multiple_task(filepath), res)
                             break
             except Exception:
-                continue
+                pass
 
         queue.put(res)
 
-    def new_task_issue(self, file_name):
-        return 'FLAG_ACTIVITY_NEW_TASK - intent flag is set. \n ' \
-               'This results in activity being loaded as a part of a new task. This can' \
-               ' be abused in the task hijacking attack. \n%s\n' \
-               'Reference: https://www.usenix.org/system/files/conference/usenixsecurity15/sec15-paper-ren-chuangang.pdf\n' \
-               % file_name
-
-    def multiple_task_issue(self, file_name):
-        return 'FLAG_ACTIVITY_MULTIPLE_TASK - intent flag is set. \n ' \
-               'This results in activity being loaded as a part of a new task. This can' \
-               ' be abused in the task hijacking attack. \n%s\n' \
-               'Reference: https://www.usenix.org/system/files/conference/usenixsecurity15/sec15-paper-ren-chuangang.pdf\n' \
-               % file_name
-
-    def getName(self):
+    @staticmethod
+    def getName():
         return "Task Hijacking"
 
-    def getCategory(self):
+    @staticmethod
+    def getCategory():
         # Currently unused, but will be used later for clubbing issues from a specific plugin (when multiple plugins run at the same time)
         return "PLUGIN ISSUES"
 
