77c5003e874efdd781b33be9e9fe854f697e0e9c
sarveshkapre <kapre.s@husky.neu.edu>
2017-08-11 17:43:15 -0700

    PR changes

--- qark/plugins/task_affinity.py
+++ qark/plugins/task_affinity.py
@@ -8,17 +8,17 @@ import lib.plyj.parser as plyj
 
 sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)) + '../lib')
 
-string = ("FLAG_ACTIVITY_{}_TASK - intent flag is set.\nThis results in activity being loaded"
+STRING = ("FLAG_ACTIVITY_{}_TASK - intent flag is set.\nThis results in activity being loaded"
           "as a part of a new task. This can be abused in the task hijacking attack.\n{}\n"
           "Reference: https://www.usenix.org/system/files/conference/usenixsecurity15/sec15-paper-ren-chuangang.pdf\n")
 
 
 def new_task(file_path):
-    return string.format("NEW", file_path)
+    return STRING.format("NEW", file_path)
 
 
 def multiple_task(file_path):
-    return string.format("MULTIPLE", file_path)
+    return STRING.format("MULTIPLE", file_path)
 
 
 class TaskAffinityPlugin(IPlugin):
@@ -30,7 +30,7 @@ class TaskAffinityPlugin(IPlugin):
 
     def target(self, queue):
         files = common.java_files
-        global filepath
+        global filepath, tree
         parser = plyj.Parser()
         tree = ''
         res = []
@@ -44,6 +44,7 @@ class TaskAffinityPlugin(IPlugin):
             except Exception as e:
                 common.logger.exception(
                     "Unable to parse the file and generate as AST. Error: " + str(e))
+                continue
 
             try:
                 for import_decl in tree.import_declarations:
@@ -57,8 +58,9 @@ class TaskAffinityPlugin(IPlugin):
                         if PluginUtil.contains(self.MULTIPLE_TASK_TASK, file_body):
                             PluginUtil.reportInfo(filepath, multiple_task(filepath), res)
                             break
-            except Exception:
-                pass
+            except Exception as e:
+                common.logger.info("Plyj parser failed while parsing the file: " + filepath + "\nError" + str(e))
+                continue
 
         queue.put(res)
 
