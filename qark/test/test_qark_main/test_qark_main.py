beb4de523628a07497a94555b2893cb58b8dbe40
Nathan Walsh <nwalsh1995@gmail.com>
2017-08-14 15:55:53 -0700

    force build

--- qark/test/test_qark_main/test_qark_main.py
+++ qark/test/test_qark_main/test_qark_main.py
@@ -3,6 +3,8 @@ from subprocess import Popen, PIPE
 
 def test_autodiscover_regression():
     process = Popen("python ../qarkMain.py --source 2 --manifest testData/goatdroid/goatdroid/AndroidManifest.xml -a 1 --exploit 0 --install 1 -t ACCEPTTERMS".split(), stdin=PIPE)
-    process.communicate('y')
+    out,err = process.communicate('y')  # install the SDK if it asks
+    print(out, err)
+    assert 1 == 2
     result = process.wait()
     assert 0 == result
