5c93723ee55fa934c66fe2592e8c688b080083e8
Nathan Walsh <nwalsh1995@gmail.com>
2017-08-14 16:53:58 -0700

    fix test

--- qark/test/test_qark_main/test_qark_main.py
+++ qark/test/test_qark_main/test_qark_main.py
@@ -1,9 +1,10 @@
 import subprocess
 
-from qark.modules.sdkManager import download_sdk
+import qark.modules.sdkManager
 
 
 def test_autodiscover_regression():
-    download_sdk()  # first download the sdk so that we can use it
-    result = subprocess.call("python ../qarkMain.py --source 2 --manifest testData/goatdroid/goatdroid/AndroidManifest.xml -a 1 --exploit 0 --install 0".split(), stdin=PIPE)
+    qark.sdkManager.common.rootDir = '..'
+    qark.modules.sdkManager.download_sdk()  # first download the sdk so that we can use it
+    result = subprocess.call("python ../qarkMain.py --source 2 --manifest testData/goatdroid/goatdroid/AndroidManifest.xml -a 1 --exploit 0 --install 0".split())
     assert 0 == result
