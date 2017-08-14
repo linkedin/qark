e096e9218fcd9d256babfc71c469e7e4f6c4514c
sarveshkapre <kapre.s@husky.neu.edu>
2017-08-14 12:05:07 -0700

    Access control plugin (#98)
    * Adding new plugins and updated modules
    
    * Updated manifest file checks and few minor corrections
    
    * Minor changes to manifest
    
    * Travis testing
    
    * modified gitignore
    
    * Deleting .idea
    
    * test case
    
    * Manifest file checks
    
    * http url test case and plugin update
    
    * Plugin and test case update - DCL
    
    * minor
    
    * log plugins and test case
    
    * log test case
    
    * http test case
    
    * http plugin
    
    * test http
    
    * test http url
    
    * removed false positives
    
    * updated test case for http url
    
    * prevent overwhelming of output on screen
    
    * code structure
    
    * Remove .DS_Store
    
    * Updated and extended Plugins
    
    * update test cases
    
    * Access Control Plugin
    
    * unit test
    
    * plugins
    
    * AST search
    
    * PR changes
    
    * PR changes
    
    * Changed method name
    
    * pep8
    
    * pep8
    
    * exception handling
    
    * PR changes

--- qark/plugins/Dynamically_Loading_Code.py
+++ qark/plugins/Dynamically_Loading_Code.py
@@ -1,72 +1,127 @@
-import sys, os, re
-sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)) + '../lib')
 from yapsy.IPlugin import IPlugin
 from plugins import PluginUtil
 from modules import common
 from lib.pubsub import pub
 import lib.plyj.parser as plyj
+import lib.plyj.model as m
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)) + '../lib')
+
+
+def broadcast_receiver(br_list):
+    STRING = ("Application dynamically registers a broadcast receiver\nApplication that register a broadcast receiver "
+              "dynamically is vulnerable to granting unrestricted access to the broadcast receiver.\nThe receiver will "
+              "be called with any broadcast Intent that matches filter.\n https://developer.android.com/reference/android/"
+              "content/Context.html#registerReceiver(android.content.BroadcastReceiver, android.content.IntentFilter)\n{}\n")
+    return STRING.format(br_list)
+
+
+def class_loader(filepath):
+    STRING = ("Application dynamically loads an external class through DexClassLoader\nFilepath: {}\n"
+              "Even though this may not be a security issue always, be careful with what you are loading.\n"
+              "Reference: https://developer.android.com/reference/dalvik/system/DexClassLoader.html \n")
+    return STRING.format(filepath)
 
 
 class DynamicallyLoadingCodePlugin(IPlugin):
     DEX_CLASS_LOADER = r'DexClassLoader'
     CLASS_LOADER = r'loadClass'
     DYNAMIC_BROADCAST_RECEIVER = r'registerReceiver'
+    global receivers_list
+    receivers_list = []
 
     def __init__(self):
         self.name = 'Dynamically loading code'
 
+    # recursive function to find the loadclass function name by traversing down the AST
+    def recursive_classloader_function(self, fields, f, res):
+        if type(fields) is m.MethodDeclaration:
+            if str(fields.name) == self.CLASS_LOADER:
+                PluginUtil.reportInfo(filepath, class_loader(filepath), res)
+            elif self.CLASS_LOADER in str(fields):
+                PluginUtil.reportInfo(filepath, class_loader(filepath), res)
+        elif type(fields) is list:
+            for tree_object_fields in fields:
+                self.recursive_classloader_function(tree_object_fields, f, res)
+        elif hasattr(fields, '_fields'):
+            for values in fields._fields:
+                self.recursive_classloader_function(getattr(fields, values), f, res)
+        return
+
+    # recursive function to find the register receiver function name by traversing down the AST
+    def recursive_register_receiver_function(self, fields, f, res):
+        if type(fields) is m.MethodDeclaration:
+            if str(fields.name) == self.DYNAMIC_BROADCAST_RECEIVER:
+                receivers_list.append(filepath)
+            elif self.DYNAMIC_BROADCAST_RECEIVER in str(fields):
+                if filepath not in receivers_list:
+                    receivers_list.append(filepath)
+        elif type(fields) is list:
+            for tree_object_fields in fields:
+                self.recursive_register_receiver_function(tree_object_fields, f, res)
+        elif hasattr(fields, '_fields'):
+            for values in fields._fields:
+                self.recursive_register_receiver_function(getattr(fields, values), f, res)
+        return
+
     def target(self, queue):
         files = common.java_files
-        global parser, tree, fileName
+        global parser, tree, filepath
         parser = plyj.Parser()
         tree = ''
         res = []
-
-        #List of Broadcast Receiver
-        list_BR = []
         count = 0
         for f in files:
             count += 1
             pub.sendMessage('progress', bar=self.name, percent=round(count * 100 / len(files)))
-            fileName = str(f)
+            filepath = str(f)
             try:
+                # Parse the java file to an AST
                 tree = parser.parse_file(f)
-            except Exception:
+            except Exception as e:
+                common.logger.exception("Unable to parse the file and generate as AST. Error: " + str(e))
                 continue
+
             try:
                 for import_decl in tree.import_declarations:
+                    # Check if DexClassLoader is called in the import statement; example import dalvik.system.DexClassLoader
                     if self.DEX_CLASS_LOADER in import_decl.name.value:
-                        if self.CLASS_LOADER in str(tree):
-                            PluginUtil.reportInfo(fileName, self.DexClassLoaderIssueDetails(fileName), res)
+                        for type_decl in tree.type_declarations:
+                            # Check class declaration within the java source code
+                            if type(type_decl) is m.ClassDeclaration:
+                                # Traverse through every field declared in the class
+                                for fields in type_decl.body:
+                                    try:
+                                        self.recursive_classloader_function(fields, f, res)
+                                    except Exception as e:
+                                        common.logger.error("Unable to run class loader plugin " + str(e))
+            except Exception as e:
+                common.logger.info("Plyj parser failed while parsing the file: " + filepath + "\nError" + str(e))
+                continue
 
-                # This will check if app register's a broadcast receiver dynamically
-                if self.DYNAMIC_BROADCAST_RECEIVER in str(tree):
-                    list_BR.append(fileName)
-
-            except Exception:
+            try:
+                for type_decl in tree.type_declarations:
+                    # Check class declaration within the java source code
+                    if type(type_decl) is m.ClassDeclaration:
+                        # Traverse through every field declared in the class
+                        for fields in type_decl.body:
+                            try:
+                                self.recursive_register_receiver_function(fields, f, res)
+                            except Exception as e:
+                                common.logger.error("Unable to run register receiver function plugin " + str(e))
+            except Exception as e:
+                common.logger.info("Plyj parser failed while parsing the file: " + filepath + "\nError" + str(e))
                 continue
 
         # Arrange the Broadcast Receivers created Dynamically in column format and store it in the variable -> Broadcast_Receiver
-        Broadcast_Receiver = "\n".join(list_BR)
+        br_list = "\n".join(receivers_list)
 
-        if list_BR:
-            PluginUtil.reportWarning(fileName, self.BroadcastReceiverIssueDetails(Broadcast_Receiver), res)
+        if receivers_list:
+            # Report the issue in the file and display it on the terminal
+            PluginUtil.reportWarning(filepath, broadcast_receiver(br_list), res)
 
         queue.put(res)
-
-    def DexClassLoaderIssueDetails(self, fileName):
-        return 'Application dynamically load an external class through DexClassLoader\n%s\n' \
-               'Even though this may not be a security issue always, be careful with what you are loading. \n' \
-               'https://developer.android.com/reference/dalvik/system/DexClassLoader.html \n' \
-               % fileName
-
-    def BroadcastReceiverIssueDetails(self, Broadcast_Receiver):
-        return 'Application dynamically registers a broadcast receiver\n' \
-               'Application that register a broadcast receiver dynamically is vulnerable to granting unrestricted access to the broadcast receiver. \n' \
-               'The receiver will be called with any broadcast Intent that matches filter.\n' \
-               'https://developer.android.com/reference/android/content/Context.html#registerReceiver(android.content.BroadcastReceiver, android.content.IntentFilter)\n' \
-               '%s\n' \
-               % Broadcast_Receiver
 
     def getName(self):
         return "Dynamically loading code"
