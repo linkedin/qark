c24a93613160ac8782d892eeef76c1c4312e64f1
Nathan Walsh <nwalsh1995@gmail.com>
2017-08-14 15:50:19 -0700

    try to install sdk

--- qark/test/test_qark_main/test_qark_main.py
+++ qark/test/test_qark_main/test_qark_main.py
@@ -1,6 +1,8 @@
-import subprocess
+from subprocess import Popen, PIPE
 
 
 def test_autodiscover_regression():
-    result = subprocess.call("python ../qarkMain.py --source 2 --manifest testData/goatdroid/goatdroid/AndroidManifest.xml -a 1 --exploit 0 --install 1 -t ACCEPTTERMS".split())
+    process = Popen("python ../qarkMain.py --source 2 --manifest testData/goatdroid/goatdroid/AndroidManifest.xml -a 1 --exploit 0 --install 1 -t ACCEPTTERMS".split(), stdin=PIPE)
+    process.communicate('y')
+    result = process.wait()
     assert 0 == result
