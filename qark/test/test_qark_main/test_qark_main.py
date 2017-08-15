9245d507de760ac94728f41f026018246f086ccb
Nathan Walsh <nwalsh1995@gmail.com>
2017-08-14 17:03:22 -0700

    fix test

--- qark/test/test_qark_main/test_qark_main.py
+++ qark/test/test_qark_main/test_qark_main.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 
 import qark.modules.sdkManager
@@ -5,6 +6,12 @@ import qark.modules.sdkManager
 
 def test_autodiscover_regression():
     qark.sdkManager.common.rootDir = '..'
+
+    if not os.path.exists(os.path.dirname("../settings.properties")):
+        f = open("../settings.properties",'w')
+        f.close()
+
+    qark.sdkManager.common.writeKey("rootDir", qark.sdkManager.common.rootDir)
     qark.modules.sdkManager.download_sdk()  # first download the sdk so that we can use it
     result = subprocess.call("python ../qarkMain.py --source 2 --manifest testData/goatdroid/goatdroid/AndroidManifest.xml -a 1 --exploit 0 --install 0".split())
     assert 0 == result
