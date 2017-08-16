8da5f00a025a6999697eef0234079a4fbede2328
sarveshkapre <kapre.s@husky.neu.edu>
2017-08-15 17:00:56 -0700

    API plugin

--- qark/plugins/api_plugin.py
+++ qark/plugins/api_plugin.py
@@ -5,26 +5,23 @@ from lib.pubsub import pub
 import sys
 import os
 import re
-import lib.plyj.parser as plyj
-import lib.plyj.model as m
 sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)) + '../lib')
 
 STRING = "API Key Found\n{}"
-
 
 def hardcoded_api_key(api_key_variable):
     return STRING.format(api_key_variable)
 
 
 class HardcodedAPIIssuesPlugin(IPlugin):
-        HARDCODED_API_KEY = r'API_KEY|api_key|API|api|key'
-      # Regex to check for API Keys containing atleast one uppercase, lowercase chracters and number
-        API_KEY_REGEX = r'^.+(?=.{20,})(?=.+\d)(?=.+[a-z])(?=.+[A-Z]).+$'
-      # Regex which is used to ignore few special characters in api keys to prevent false positives
-        SPECIAL_CHAR_REGEX = r'^.+(?=.+[!$%^~]).+$'
+    HARDCODED_API_KEY = r'API_KEY|api_key|API|api|key'
+    # Regex to check for API Keys containing atleast one uppercase, lowercase chracters and number
+    API_KEY_REGEX = r'^.+(?=.{20,})(?=.+\d)(?=.+[a-z])(?=.+[A-Z]).+$'
+    # Regex which is used to ignore few special characters in api keys to prevent false positives
+    SPECIAL_CHAR_REGEX = r'^.+(?=.+[!$%^~]).+$'
 
     def __init__(self):
-        self.name = 'Detect exposed logs'
+        self.name = 'Hardcoded API Keys'
 
     def target(self, queue):
         files = common.java_files
@@ -39,20 +36,21 @@ class HardcodedAPIIssuesPlugin(IPlugin):
             with open(f, 'r') as fi:
                 file_content = fi.read()
 
-            for line in file_content.splitlines():
-                if re.match(self.API_KEY_REGEX, line):
-                 # Check if special character is present in the line. If "Yes, then ignore.
-                    if re.match(self.SPECIAL_CHAR_REGEX, line):
-                        pass
-                else:
-                    if line not in api_key_list:
-                        api_key_list.append(line)
+            # for line in file_content.splitlines():
+            if re.match(self.API_KEY_REGEX, file_content):
+             # Check if special character is present in the line. If "Yes, then ignore.
+                if re.match(self.SPECIAL_CHAR_REGEX, file_content):
+                    pass
+            else:
+                if file_path not in api_key_list:
+                    api_key_list.append(file_path)
 
         api_key_variable = "\n".join(api_key_list)
 
         if api_key_list:
             PluginUtil.reportInfo(file_path, hardcoded_api_key(api_key_variable), res)
 
+        queue.put(res)
 
     @staticmethod
     def getName():
