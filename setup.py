4fc3ca94fa27b876e2d2ebc7072ceaa91e837260
Neil Davenport <ndavenport@linkedin.com>
2017-11-02 09:40:45 -0700

    Moving to a minor version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 
 setup(
     name = "qark",
-    version = "2.0.0",
+    version = "1.3.0",
     packages = ['qark/modules','qark/lib', 'qark'],
     include_package_data = True,
 #     scripts = ['qark.py'],
