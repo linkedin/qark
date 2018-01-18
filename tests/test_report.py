fd226682d816cd0bf81c2402e85cae77ba7122e4
Neil Davenport <ndavenport@linkedin.com>
2018-01-18 14:59:09 -0800

    Adding in missing templates

--- tests/test_report.py
+++ tests/test_report.py
@@ -28,7 +28,7 @@ def test_report_html_defaults():
     report.issues.remove(issue)
     assert os.path.exists(os.path.join(DEFAULT_REPORT_PATH, 'report.html'))
     # We remove the report, to clean up after ourselves
-    #os.remove(os.path.join(DEFAULT_REPORT_PATH, 'report.html'))
+    os.remove(os.path.join(DEFAULT_REPORT_PATH, 'report.html'))
 
 
 def test_report_xml_defaults():
@@ -40,7 +40,7 @@ def test_report_xml_defaults():
     report.issues.remove(issue)
     assert os.path.exists(os.path.join(DEFAULT_REPORT_PATH, 'report.xml'))
     # We remove the report, to clean up after ourselves
-    #os.remove(os.path.join(DEFAULT_REPORT_PATH, 'report.xml'))
+    os.remove(os.path.join(DEFAULT_REPORT_PATH, 'report.xml'))
 
 
 def test_report_csv_defaults():
@@ -52,7 +52,7 @@ def test_report_csv_defaults():
     report.issues.remove(issue)
     assert os.path.exists(os.path.join(DEFAULT_REPORT_PATH, 'report.csv'))
     # We remove the report, to clean up after ourselves
-    #os.remove(os.path.join(DEFAULT_REPORT_PATH, 'report.csv'))
+    os.remove(os.path.join(DEFAULT_REPORT_PATH, 'report.csv'))
 
 
 def test_report_json_defaults():
@@ -64,7 +64,7 @@ def test_report_json_defaults():
     report.issues.remove(issue)
     assert os.path.exists(os.path.join(DEFAULT_REPORT_PATH, 'report.json'))
     # We remove the report, to clean up after ourselves
-    #os.remove(os.path.join(DEFAULT_REPORT_PATH, 'report.json'))
+    os.remove(os.path.join(DEFAULT_REPORT_PATH, 'report.json'))
 
 
 def test_report_html_custom_template():
