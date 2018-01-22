86bb0b34e7e59ee9faceb766576479b0704d4194
Dan Haugh <dhaugh@linkedin.com>
2018-01-22 14:31:16 -0800

    RB for ecb plugin

--- qark/plugins/crypto/ecb_cipher_usage.py
+++ qark/plugins/crypto/ecb_cipher_usage.py
@@ -2,7 +2,6 @@ import os
 import logging
 
 import re
-from plyj.parser import Parser
 import javalang
 
 from qark.scanner.plugin import BasePlugin
@@ -18,11 +17,9 @@ class ECBCipherCheck(BasePlugin):
                             description=("ECB mode is an insecure encryption technique and prone to data leakage"))
 
         self.severity = Severity.VULNERABILITY
-        self.parser = Parser()
         self.tree = None
 
     def _process_file(self, filepath):
-        pass
         try:
             with open(filepath, 'r') as f:
                 body = f.read()
@@ -32,23 +29,24 @@ class ECBCipherCheck(BasePlugin):
 
         try:
             tree = javalang.parse.parse(body)
-            method_invocations = tree.filter(javalang.tree.MethodInvocation)
-            for node_path, method_invocation_node in method_invocations:
-                try:
-                    method_name = method_invocation_node.member
-                    encryption_type = method_invocation_node.arguments[0].value
-                    qualifier = method_invocation_node.qualifier  # the thing that getInstance is called on
-                    if method_name == 'getInstance' and qualifier == 'Cipher' and re.search(r'.*/ECB/.*',
-                                                                                            encryption_type):
-                        description = "getInstance should not be called with ECB as the cipher mode, as it is insecure."
-                        self.issues.append(
-                            Issue(self.category, self.name, self.severity, description, file_object=filepath))
-                except Exception:
-                    continue
         except Exception:
             log.exception("Couldn't parse the java file: %s", filepath)
 
+        method_invocations = tree.filter(javalang.tree.MethodInvocation)
+        for node_path, method_invocation_node in method_invocations:
+            try:
+                method_name = method_invocation_node.member
+                encryption_type = method_invocation_node.arguments[0].value
+                qualifier = method_invocation_node.qualifier  # the thing that getInstance is called on
+                if method_name == 'getInstance' and qualifier == 'Cipher' and re.search(r'.*/ECB/.*',
+                                                                                        encryption_type):
+                    description = "getInstance should not be called with ECB as the cipher mode, as it is insecure."
+                    self.issues.append(
+                        Issue(self.category, self.name, self.severity, description, file_object=filepath))
+            except Exception:
+                continue
+
     def run(self, files, apk_constants=None):
-        relevant_files = [file for file in files if os.path.splitext(file)[1] == '.java']
+        relevant_files = [file_path for file_path in files if os.path.splitext(file)[1] == '.java']
         for file in relevant_files:
             self._process_file(file)
