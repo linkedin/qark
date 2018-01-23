421d5e1a5f746cc2c35cc6a1319c7833db799441
Nathan Walsh <nwalsh@linkedin.com>
2018-01-23 14:00:02 -0800

    add scanner checks for intent

--- tests/test_scanner/test_scanner.py
+++ tests/test_scanner/test_scanner.py
@@ -7,13 +7,21 @@ def test_run(scanner):
 
 
 def test_run_manifest_checks(scanner):
+    scanner.issues = []
     scanner._run_manifest_checks()
     assert 0 < len(scanner.issues)
 
     # this should hit the other code path where
     #   manifest_path is already set
+    scanner.issues = []
     scanner._run_manifest_checks()
     assert 0 < len(scanner.issues)
+
+
+def test_run_intent_checks(scanner):
+    scanner.issues = []
+    scanner._run_intent_checks()
+    assert 0 == len(scanner.issues)  # goatdroid doesnt have any of these vulnerabilities
 
 
 def test_scanner_singleton(decompiler):
