30c80515f961aa0138aad9f919213e5897f8aea8
Nathan Walsh <nwalsh1995@gmail.com>
2018-03-21 16:26:26 -0700

    add tests for phone identifier

--- tests/test_plugins/test_file_plugins/test_file_plugins.py
+++ tests/test_plugins/test_file_plugins/test_file_plugins.py
@@ -1,4 +1,5 @@
 from qark.plugins.file.file_permissions import FilePermissions, WORLD_READABLE_DESCRIPTION, WORLD_WRITEABLE_DESCRIPTION
+from qark.plugins.file.phone_identifier import PhoneIdentifier
 from qark.issue import Severity
 
 import os
@@ -16,3 +17,9 @@ def test_file_permissions():
     assert Severity.WARNING == plugin.issues[1].severity
     assert WORLD_WRITEABLE_DESCRIPTION == plugin.issues[1].description
 
+
+def test_phone_identifier(test_java_files):
+    plugin = PhoneIdentifier()
+    plugin.run([os.path.join(test_java_files,
+                             "phone_identifier.java")])
+    assert 1 == len(plugin.issues)
