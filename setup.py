b0b868fa6f010f7fe8a9a8e93e0ed553f728b5e9
nafuti <35099140+nafuti@users.noreply.github.com>
2018-06-29 11:47:18 +0300

    Setup error on pip<=10.0 (#233)
    Add an extra check for pip 10 version to prevent setup errors

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
+
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 with open('requirements.txt') as f:
     required = f.read().splitlines()
