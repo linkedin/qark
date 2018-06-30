a9afb7909df1edfd36db017b2564b921ca6aa858
Nathan Walsh <nwalsh1995@gmail.com>
2018-06-29 22:49:15 -0700

    fix up exploit apk and write more docs around it

--- README.rst
+++ README.rst
@@ -26,3 +26,19 @@ With `requirements.txt` (security checks on requirements)
   ~ pip install .
   ~ qark --help
 
+
+Exploit APK
+###########
+QARK can generate a basic exploit APK for a few of the vulnerabilities that have been found. The following vulnerabilities
+are included in the exploit APK:
+
+1. List
+2. Here
+
+To generate the exploit APK there are a few steps to follow.
+
+1. Install the android SDK, you can get it under the 'command line tools': https://developer.android.com/studio/#downloads
+2. Unzip the android SDK
+3. Go into the new directory and generate the licenses with `bin/sdkmanager --licenses`
+4. Make sure the generated licenses are in the android SDK directory.
+5. Install the SDK and the proper build-tools version: `bin/sdkmanager --install "platforms;android-21" "sources;android-21" "build-tools;21.1.2"`
--- setup.py
+++ setup.py
@@ -7,10 +7,16 @@ template_files = [os.path.join("qark", "templates", template)
                   for template in os.listdir(TEMPLATE_DIR) if os.path.splitext(template)[1] == ".jinja"]
 
 
-LIB_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "qark", "lib")
+QARK_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "qark")
+LIB_DIR = os.path.join(QARK_DIR, "lib")
 DECOMPILERS_DIR = os.path.join(LIB_DIR, "decompilers")
 jar_files = [os.path.join(DECOMPILERS_DIR, jar_file)
              for jar_file in os.listdir(DECOMPILERS_DIR) if os.path.splitext(jar_file)[1] == ".jar"]
+
+
+exploit_apk_files = [os.path.join(dir_path, filename)
+                     for dir_path, _, files in os.walk(os.path.join(QARK_DIR, "exploit_apk"))
+                     for filename in files]
 
 with open('requirements.txt') as f:
     all_lines = f.read().splitlines()
@@ -20,7 +26,7 @@ setup(
     name="qark",
     version="2.0",
     packages=find_packages(),
-    package_data={"qark": jar_files},  # include the jd_core.jar file
+    package_data={"qark": jar_files + exploit_apk_files},  # include the jd_core.jar file
     include_package_data=True,  # includes template files in qark/templates
     install_requires=required,
     # metadata for upload to PyPI
